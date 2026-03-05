6a7f00498bb99546cc9ee883916267adc02c9971
hamzapos2026-ai <hamzapos2026@gmail.com>
2026-03-06 03:00:47 +0500

    First coomit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-red-400">
+        Tailwind CSS Working 🚀
+      </h1>
+    </div>
+  )
+}
